1f6b07371022d1092342d551a8d9837d4508df8f
libiao <2865866423@qq.com>
2020-11-09 18:40:25 +0800

    eureka集群

--- cloud-consumer-order80/src/main/java/com/li/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/li/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.li.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: 一条IT
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
--- cloud-eureka-server7001/src/main/java/com/li/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/li/springcloud/EurekaMain7001.java
@@ -1,10 +1,17 @@
 package com.li.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @Author: 一条IT
  * @Date: 2020/11/9 13:19
  */
-
-
+@EnableEurekaServer
+@SpringBootApplication
 public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
 }
