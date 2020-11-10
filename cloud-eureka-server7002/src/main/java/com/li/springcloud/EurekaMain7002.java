4f1e1e7663d35776c775aaeb294a1c349e2d6100
libiao <2865866423@qq.com>
2020-11-10 11:58:48 +0800

    eureka集群

---
+++ cloud-eureka-server7002/src/main/java/com/li/springcloud/EurekaMain7002.java
@@ -0,0 +1,18 @@
+package com.li.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: 一条IT
+ * @Date: 2020/11/9 19:02
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
