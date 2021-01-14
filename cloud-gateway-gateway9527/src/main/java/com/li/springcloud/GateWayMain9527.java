93f645d935f75ba4ca0267e5c84aef926973718f
libiao <2865866423@qq.com>
2021-01-14 12:34:41 +0800

    gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/li/springcloud/GateWayMain9527.java
@@ -0,0 +1,16 @@
+package com.li.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 一条IT
+ * @Date: 2021/1/13 14:47
+ */
+
+@SpringBootApplication
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
